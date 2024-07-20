1285491ae73ad8080dfcf067114d737c5f1f47a1
Devon <larsdevonn@gmail.com>
2024-07-20 13:26:13 -0700

    Added github and linkedin links for about page

--- react-vite/src/components/About/About.jsx
+++ react-vite/src/components/About/About.jsx
@@ -1,9 +1,21 @@
-const About = () => {
+import './About.css'
+import {NavLink} from "react-router-dom";
+
+function About () {
 
     return (
         <>
+            <div className='main'>
+                <div className='about-container'>
+                    <div id='about-wrapper'>
+                        <h2 id={'about-name'}>Lars-Devon Nilsson</h2>
+                        <NavLink id={'about-github'} to={'https://github.com/ldevonn'}>Github</NavLink>
+                        <NavLink id={'about-linkedin'} to={'https://www.linkedin.com/in/lars-devon-nilsson-1989781b0'}>Linkedin</NavLink>
+                    </div>
+                </div>
+            </div>
         </>
-    );
+    )
 }
 
-export default About;
+export default About
